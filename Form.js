56c22178a5ef5ae88f59c27c9f9b03d5b4724613
Suman37-2 <72697297+Suman37-2@users.noreply.github.com>
2021-03-21 11:58:49 +0530

    Add files via upload

--- Form.js
+++ Form.js
@@ -1,30 +1,37 @@
 class Form{
- constructor(){}
+ constructor(){
+  this.input = createInput("Name");
+  this.button = createButton('Play');
+  this.greeting = createElement('h2');
+  this.title = createElement('h2');
+ }
+
+ hide(){
+  this.greeting.hide();
+  this.button.hide(); 
+  this.input.hide();
+  this.title.hide();  
+ }
  
  display(){
-  var title = createElement('h2');
-  title.html("Car Racing Game");
-  title.position(150,0);
-  
-  var input = createInput("Name");
-  var button = createButton('Play');
-  var greeting = createElement('h3');
+  this.title.html("Car Racing Game");
+  this.title.position(displayWidth/2 - 50, 0);
 
-  input.position(130,160);
-  button.position(250,200);
+   this.input.position(displayWidth/2 - 40, displayHeight/2 - 80);
+   this.button.position(displayWidth/2 + 30, displayHeight/2);
 
-  button.mousePressed(function(){
-   input.hide();
-   button.hide();
+   this.button.mousePressed(()=> {
+   this.input.hide();
+   this.button.hide();
    
-   var name=input.value();
-
+   player.name=this.input.value();
    playerCount+=1;
-   player.update(name);
+   player.index = playerCount;
+   player.update();
    player.updateCount(playerCount);
 
-   greeting.html("Hello " + name);
-   greeting.position(130,160);
+   this.greeting.html("Hello " + player.name);
+   this.greeting.position(displayWidth/2 - 70, displayHeight/4);
   })
  }
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var gameState=0,playerCount,database;
-var form,player,game;
+var form,player,game,allPlayers;
+var cars,car1,car2,car3,car4;
 
 function setup(){
-createCanvas(400,400);
+createCanvas(displayWidth-20,displayHeight-30);
 database=firebase.database(); 
 
 game = new Game();
@@ -13,5 +14,12 @@ game.start();
 function draw(){
 background("pink");
 
+if(playerCount === 4){
+   game.update(1); 
+}
+if(gameState === 1){
+   clear();
+   game.play(); 
+}
 
 }
